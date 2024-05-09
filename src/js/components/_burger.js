3119899bb38811d281672b0c8d7b821e4dee30b7
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-05-09 22:09:48 +0300

    to reviews

--- src/js/components/_burger.js
+++ src/js/components/_burger.js
@@ -1,24 +1,27 @@
 const navigation = document.querySelector('.nav');
 const page = document.querySelector('.page');
+const header = document.querySelector('.header');
 
-navigation?.addEventListener('click', onBurgerBtnClick);
+navigation ? .addEventListener('click', onBurgerBtnClick);
 
 function onBurgerBtnClick(e) {
   if (!e.target.parentNode.matches('.burger') && !e.target.matches('.burger')) {
     return;
   }
 
-  this?.classList.remove('closed');
-  page?.classList.add('open');
+  this ?.classList.remove('closed');
+  page ?.classList.add('open');
+  header ?.classList.add('active');
 
-  if (this?.matches('.opened')) {
-    this?.classList.add('closed');
-    page?.classList.remove('open');
+  if (this ?.matches('.opened')) {
+    this ?.classList.add('closed');
+    page ?.classList.remove('open');
+    header ?.classList.remove('active');
 
     setTimeout(() => {
-      this?.classList.remove('opened');
+      this ?.classList.remove('opened');
     }, 100);
   }
 
-  this?.classList.add('opened');
+  this ?.classList.add('opened');
 }
